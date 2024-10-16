8e1cdc807bfcc53906e87916d561190a4f0d2965
Ollie Gutierrez <ollie.f.gutierrez@gmail.com>
2024-10-16 14:51:02 +0000

    changed leader to space

--- config/default.nix
+++ config/default.nix
@@ -6,4 +6,14 @@
   	./ui
   	./utils
   ];
+
+  globals.mapleader = " ";
+
+  keymaps = [
+    {
+      action = "<Nop>";
+      key = "<Space>";
+      options = { noremap = true; silent = true; };
+    }
+  ];
 }
